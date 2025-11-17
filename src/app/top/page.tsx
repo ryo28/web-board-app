45e47eea6038f5ba195d1eebc040160613813439
ryo28 <wakisan1@ezweb.ne.jp>
2025-11-17 17:24:40 +0900

    ✨ トップコンポーネントのスタイルを追加し、レイアウトを整形

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Top() {
-  return (
-   <div>top</div>
-  );
+    return (
+        <div className="top-0 flex justify-center border bg-amber-50 text-center">
+            top
+        </div>
+    );
 }
